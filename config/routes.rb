4457414b2dcdb44abc36040af98ed84962f250de
LondonBishop <londonbishop@codecutters.co.uk>
2019-03-19 10:15:28 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get "/authors/new", to: "authors#new", as: "new_author"
   get "/authors/:id", to: "authors#show", as: "author"
   post "/authors", to: "authors#create"
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   get "/posts/:id", to: "posts#show", as: "post"
   get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
   patch "/posts/:id", to: "posts#update"
+  
 end
